99d18783279563e1b866cd88cab2a1877b7aa136
today16 <k101620275@yahoo.com.tw>
2022-09-20 15:29:07 +0800

    ok

---
+++ 111-1PC3/Test.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC3
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_Clothes = 300;
+            int i_Hat = 350;
+            int i_Pants = 400;
+            int s;
+            s = (i_Clothes * 7) + (i_Hat * 8) + (i_Pants * 9); 
+            Response.Write(s);
+        }
+    }
+}
